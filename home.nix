5e6b6042fff9155e83564d32475c9fec7a4ad148
lratt <104203130+lratt@users.noreply.github.com>
2024-09-19 12:21:16 +0200

    add starship prompt

--- home.nix
+++ home.nix
@@ -58,6 +58,13 @@
     enableCompletion = true;
   };
 
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
